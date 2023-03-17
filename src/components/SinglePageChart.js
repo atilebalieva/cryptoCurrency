e30993a8e881ba13352d801940bb9b4270d1e6b1
atilebalieva <aitilebalieva@gmail.com>
2023-03-17 18:52:11 -0500

    Started to style Chart and added SelectButton comp-t to chart

--- src/components/SinglePageChart.js
+++ src/components/SinglePageChart.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Loader from "./Loader";
+import { Chart as ChartJS } from "chart.js/auto";
+import SelectButton from "./SelectButton";
 
 function SinglePageChart({ id }) {
   const [historicalData, setHistoricalData] = useState();
+
+  useEffect(() => {}, []);
 
   return <div></div>;
 }
